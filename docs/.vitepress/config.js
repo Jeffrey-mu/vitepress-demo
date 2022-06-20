fd3a1a2cd3015b13e26af1bac116bd44b0a08915
unknown <Jeffrey-mu>
2022-06-20 09:47:06 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vitepress';
+export default defineConfig({
+    lang: 'en-US',
+    title: 'VitePress',
+    description: 'Vite & Vue powered static site generator.',
+});
